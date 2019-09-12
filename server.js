623713dc6cd64110995a900dd93e35e844686e91
Dora Belme <contact.dorabelme@gmail.com>
2019-09-12 16:46:31 -0400

    added data access functions and endpoints for stretch goal

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const SchemeRouter = require('./schemes/scheme-router.js');
+const RecipesRouter = require('./recipes/recipesRouter.js');
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/schemes', SchemeRouter);
+server.use('/api/recipes', RecipesRouter);
 
 module.exports = server;
